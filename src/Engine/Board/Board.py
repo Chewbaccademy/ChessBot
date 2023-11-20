71b05c865433be815d143a4845d459db19e1a555
Chewbaccademy <kilian.debraux@gmail.com>
2023-11-20 21:26:37 +0100

    Feat : creating Board classes

---
+++ src/Engine/Board/Board.py
@@ -0,0 +1,10 @@
+
+
+class Board:
+    """
+        Board:
+        Class representing the whole chess board
+        Useful to interact with board and board contents (like squares, rows, etc.)
+    """
+    def __init__(self) -> None:
+        pass
---
+++ src/Engine/Board/Square.py
@@ -0,0 +1,9 @@
+
+
+class Square:
+    """
+        Board:
+        Class representing a square in the chess board
+    """
+    def __init__(self) -> None:
+        pass
