b7fd460ca775c6a36471bc1b578257f4e167898b
Chewbaccademy <kilian.debraux@gmail.com>
2023-11-21 18:43:15 +0100

    Better : rank number not glued to terminal side

--- src/Engine/Player/GUI/TerminalGUI.py
+++ src/Engine/Player/GUI/TerminalGUI.py
@@ -8,9 +8,9 @@ class TerminalGUI(GUI):
     
     
     def display_board(self, board:Board):
-        txt_board = " " + "".join(["  " + file_name + " " for file_name in board.file_list]) + "\n"
-        txt_board += " +" + "---+" * board.file_number + "\n"
+        txt_board = "   " + "".join(["  " + file_name + " " for file_name in board.file_list]) + "\n"
+        txt_board += "   +" + "---+" * board.file_number + "\n"
         for rank in range(board.rank_number, 0, -1):
-            txt_board += f"{rank}|" + "".join(["   |" for _ in range(board.file_number)]) + "\n"
-            txt_board += " +" + "---+" * board.file_number + "\n"
+            txt_board += f" {rank} |" + "".join(["   |" for _ in range(board.file_number)]) + "\n"
+            txt_board += "   +" + "---+" * board.file_number + "\n"
         print(txt_board)
