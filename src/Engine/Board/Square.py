6974f5264077abd23e2c73e4c2aebc09c8eeafc0
Chewbaccademy <kilian.debraux@gmail.com>
2023-11-28 21:21:49 +0100

    Feat : can merge 2 SquareCollections

--- src/Engine/Board/Square.py
+++ src/Engine/Board/Square.py
@@ -39,4 +39,23 @@ class SquareCollection:
                 raise ValueError("If the collection is not a squared board, the dimensions must be set")
             
         return SquareCollection(self.square_list[id * nb_files : id * nb_files + nb_files], (1, nb_files))
+    
+    def __getitem__(self, index:int|tuple) -> Square:
+        if isinstance(index, int):
+            return self.get_rank_by_id(index)
+        
+        elif isinstance(index, tuple):
+            ret_value = SquareCollection()
+            for i in range(index[0], index[1], index[2]):
+                ret_value += self.get_rank_by_id(i)
+            return ret_value
+                
+            
+        raise ValueError("The index must be an integer or a slice when used like SquareCollection[index]")
+    
+    def __add__(self, other):
+        if isinstance(other, self.__class__):
+            return SquareCollection(self.square_list + other.square_list)
+        
+        raise ValueError(f"Cannot use the operator + between a SquareCollection and a {type(other)}")
         
