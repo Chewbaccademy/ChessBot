f1c609018471edd8dd9f7b68dccc070f90ee242f
Chewbaccademy <kilian.debraux@gmail.com>
2024-04-22 22:03:25 +0200

    generate columns completed

--- src/evaluator/Dataset.py
+++ src/evaluator/Dataset.py
@@ -1,4 +1,5 @@
 from pandas import DataFrame, np
+from utils import File
 
 
 
@@ -23,9 +24,45 @@ class Dataset:
         self.data.columns = columns
     
     def __generate_columns(self) -> list:
+        # add one column to check for each piece in each square
         cols = []
         for p in self.__pieces:
             for r in self.__rank:
                 for f in self.__files:
                     cols.append(f"is_{p}_in_{f}{r}")
+                    
+        # add other FEN information
+        cols.append('is_white_turn')
+        cols.append('can_k_castle')
+        cols.append('can_q_castle')
+        cols.append('can_K_castle')
+        cols.append('can_Q_castle')
+        cols.append('number_of_en_passant_possible')
+        cols += self.__generate_en_passant_columns()
+        cols.append('halfmoves_since_non_reversible_move')
+        cols.append('number_of_moves')
+        
         return cols
+    
+    def __generate_en_passant_columns(self):
+        ep_cols = []
+        
+        for r in self.__rank:
+            for f in self.__files:
+                ep_cols.append(f"is_en_passant_in_{f}{r}")
+                
+        return ep_cols
+    
+    def get_line_from_fen(self, fen:str):
+        
+        line = dict()
+        
+        
+        fen_parts = fen.split(' ')
+        # get board information
+        board = fen_parts.pop(0)
+        ranks = board.split('/')
+        
+        for rank in ranks:
+            for info in rank:
+                pass
